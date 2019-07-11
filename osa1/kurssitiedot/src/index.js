45b21a7c5b3e3d38b2155cfb92364b6a51e3a0be
Valtteri Valtanen <valtteri.valtanen@tuni.fi>
2019-07-11 12:35:04 +0300

    kurssitiedot step2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -15,11 +15,20 @@ const App = () => {
             <h1>{props.course}</h1>
         )
     }
-    const Content = (props) => {
+    const Part = (props) => {
         return (
             <p>
                 {props.part} {props.exercises}
             </p>
+        )
+    }
+    const Content = (props) => {
+        return (
+            <div>
+                <Part part={part1} exercises={exercises1} />
+                <Part part={part2} exercises={exercises2} />
+                <Part part={part3} exercises={exercises3} />
+            </div>
         )
     }
     const Total = (props) => {
@@ -31,9 +40,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content part={part1} exercises={exercises1} />
-            <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />
+            <Content />
             <Total count={exercises1 + exercises2 + exercises3} />
         </div>
     )
