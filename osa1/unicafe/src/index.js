f88514d0406c7d92aed0c42f774da524d6680453
Valtteri Valtanen <valtteri.valtanen@tuni.fi>
2019-07-18 13:18:18 +0300

    unicafe step6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -9,7 +9,7 @@ const Statistics = ({good, neutral, bad}) => {
     const total = good+neutral+bad
     const average = (good-bad)/total
     const positive = good/total * 100 + ' %'
-    
+
     if (total === 0) {
         return (
             <div>
@@ -18,14 +18,28 @@ const Statistics = ({good, neutral, bad}) => {
         )
     }
     return (
-        <div>
-            <Statistic text='Good' value={good} />
-            <Statistic text='Neutral' value={neutral} />
-            <Statistic text='Bad' value={bad} />
-            <Statistic text='All' value={total} />
-            <Statistic text='Average' value={average} />
-            <Statistic text='Positive' value={positive} />
-        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>good</td><td>{good}</td>
+                </tr>
+                <tr>
+                    <td>neutral</td><td>{neutral}</td>
+                </tr>
+                <tr>
+                    <td>bad</td><td>{bad}</td>
+                </tr>
+                <tr>
+                    <td>all</td><td>{total}</td>
+                </tr>
+                <tr>
+                    <td>average</td><td>{average}</td>
+                </tr>
+                <tr>
+                    <td>positive</td><td>{positive}</td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
 const App = () => {
