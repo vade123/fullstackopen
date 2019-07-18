ea6ce12954f53e69cf30c916ef8b76f8c3a34cd0
Valtteri Valtanen <valtteri.valtanen@tuni.fi>
2019-07-18 13:04:08 +0300

    unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -4,29 +4,38 @@ import ReactDOM from 'react-dom'
 const Title = ({title}) => <h1>{title}</h1>
 const Button = ({handleClick, text}) => <button onClick = {handleClick}>{text}</button>
 const Counter = ({type, count}) => <div>{type} {count}</div>
-const Positive = ({total, good}) => <div>Positive {good/total * 100} %</div>
 
+const Statistics = ({good, neutral, bad}) => {
+    const total = good+neutral+bad
+    const average = good-bad
+    return (
+        <div>
+            <Title title='Statistics' />
+            <Counter type='Good' count={good} />
+            <Counter type='Neutral' count={neutral} />
+            <Counter type='Bad' count={bad} />
+            <Counter type='All' count={total} />
+            <Counter type='Average' count={average/total} />
+            <div>Positive {good/total * 100} %</div>
+        </div>
+    )
+}
 const App = () => {
     // tallenna napit omaan tilaansa
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const [total, setTotal] = useState(0)
-    const [average, setAverage] = useState(0)
 
     const handleClick = (type) => {
         if (type === 'good') {
             setGood(good+1)
-            setAverage(average+1)
         }
         else if (type === 'neutral') {
             setNeutral(neutral+1)
         }
         else {
             setBad(bad+1)
-            setAverage(average-1)
         }
-        setTotal(total+1)
     }
 
     return (
@@ -35,13 +44,7 @@ const App = () => {
             <Button handleClick={() => handleClick('good')} text='Good' />
             <Button handleClick={() => handleClick('neutral')} text='Neutral' />
             <Button handleClick={() => handleClick('bad')} text='Bad' />
-            <Title title='Statistics' />
-            <Counter type='Good' count={good} />
-            <Counter type='Neutral' count={neutral} />
-            <Counter type='Bad' count={bad} />
-            <Counter type='All' count={total} />
-            <Counter type='Average' count={average/total} />
-            <Positive total={total} good={good} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
