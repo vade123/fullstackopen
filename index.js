8b77387c644f87a606432e3c051209799ed3fc79
Valtteri Valtanen <vade1997@gmail.com>
2019-11-27 12:37:28 +0200

    3.7. puhelinluettelon backend step7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 
 let persons = [
@@ -26,6 +27,7 @@ let persons = [
 ];
 
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 app.get('/api/persons', (req, res) => {
     res.json(persons);
