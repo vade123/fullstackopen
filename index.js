372f693dcc70ed7667513b763b0836fa2e831f54
Valtteri Valtanen <vade1997@gmail.com>
2019-11-30 17:04:42 +0200

    deleted persons -array from the code

--- index.js
+++ index.js
@@ -7,29 +7,6 @@ const Person = require('./models/person');
 
 const app = express();
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "124251233",
-        id: 1
-      },
-      {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-      },
-      {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-      },
-      {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-      }
-];
-
 app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.json());
