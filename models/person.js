9c1d5e88dd8781dc2d5aade2d57cab2271c3e2b1
Valtteri Valtanen <vade1997@gmail.com>
2019-12-11 13:29:12 +0200

    3.20 puhelinluettelo ja tietokanta step8

--- models/person.js
+++ models/person.js
@@ -19,11 +19,13 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minlength: 3
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8
     }
 });
 
