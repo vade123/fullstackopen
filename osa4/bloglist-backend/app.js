bb644c684dea6ed32194075ff9ff41edc9833aa4
Valtteri Valtanen <vade1997@gmail.com>
2019-12-19 16:29:08 +0200

    4.2 blogilista step2, models/blog.js

--- osa4/bloglist-backend/app.js
+++ osa4/bloglist-backend/app.js
@@ -2,17 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
