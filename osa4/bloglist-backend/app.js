fe500127837a69e47146cde8107168167147e10c
Valtteri Valtanen <vade1997@gmail.com>
2019-12-19 17:07:50 +0200

    4.2 blogilista step2 done, controllers/blogs.js added

--- osa4/bloglist-backend/app.js
+++ osa4/bloglist-backend/app.js
@@ -4,30 +4,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose');
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
 
-app.use(cors())
-app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/blogs', blogsRouter);
 
 module.exports = app
--- osa4/bloglist-backend/models/blog.js
+++ osa4/bloglist-backend/models/blog.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 
-const blogSchema = mongoose.Schema({
+const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
     url: String,
     likes: Number
 });
 
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id
-    delete returnedObject._id
-    delete returnedObject.__v
-    }
-});
-
 const Blog = mongoose.model('Blog', blogSchema);
 
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 module.exports = Blog;
