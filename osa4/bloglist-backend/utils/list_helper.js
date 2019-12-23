bb3200a40686188c8c1798ac145f48c65f8948cd
Valtteri <vade1997@gmail.com>
2019-12-23 16:08:42 +0200

    4.4 apufunktioita ja yksikkötestejä step2

--- osa4/bloglist-backend/utils/list_helper.js
+++ osa4/bloglist-backend/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1;
 };
-
+const totalLikes = (blogs) => {
+    const reducer = (sum, cv) => sum + cv.likes;
+    if (blogs.length === 0) {
+        return 0;
+    } else {
+        return blogs.reduce(reducer, 0);
+    }
+};
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 };
