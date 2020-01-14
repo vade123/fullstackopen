6e679d50fcfc0cbf283dd61ec9d07b9828c5a890
Valtteri Valtanen <vade1997@gmail.com>
2020-01-14 18:47:04 +0200

    4.11*: blogilistan testit, step4

--- osa4/bloglist-backend/tests/blogs_api.test.js
+++ osa4/bloglist-backend/tests/blogs_api.test.js
@@ -59,6 +59,24 @@ test('a valid blog can be added', async () => {
     expect(titles).toContain('Type wars');
 });
 
+test('likes are initialized to 0', async () => {
+    const newBlog = {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    };
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    blogsAtEnd.forEach(blog => expect(blog.likes).toBeGreaterThanOrEqual(0));
+
+});
+
 afterAll(() => {
     mongoose.connection.close();
 });
