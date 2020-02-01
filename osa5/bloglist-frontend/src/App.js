44e259f0e04eb0bbee4afe7447c0a0c89220be73
Valtteri <vade1997@gmail.com>
2020-02-01 19:22:06 +0200

    5.5 blogilistan frontend, step5

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -5,6 +5,7 @@ import BlogForm from './components/BlogForm';
 import Notification from './components/Notification';
 import blogService from './services/blogs';
 import loginService from './services/login';
+import Toggable from './components/Toggable';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -52,31 +53,31 @@ const App = () => {
   };
 
   const loginForm = () => (
-    <div>
-      <h2>log in to application</h2>
-      <Notification message={notification[0]} color={notification[1]} />
-      <form onSubmit = {handleLogin}>
-        <div>
-          username
-          <input 
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)} 
-          />
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </div>
-      </form>
-    </div>
+      <div>
+        <h2>log in to application</h2>
+        <Notification message={notification[0]} color={notification[1]} />
+          <form onSubmit = {handleLogin}>
+            <div>
+              username
+              <input 
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)} 
+              />
+              <div>
+                password
+                <input
+                  type="password"
+                  value={password}
+                  name="Password"
+                  onChange={({ target }) => setPassword(target.value)}
+                />
+              </div>
+              <button type="submit">login</button>
+            </div>
+          </form>
+      </div>
   );
   
   const postBlog = async (newBlog) => {
@@ -97,7 +98,9 @@ const App = () => {
         <button onClick={handleLogout}>logout</button>
       </div>
       <br />
-      <BlogForm postBlog={postBlog} />
+      <Toggable buttonLabel="new blog">
+        <BlogForm postBlog={postBlog} />
+      </Toggable>
       {blogs.map(blog => 
         <Blog key={blog.id} blog={blog} />)}
     </div>
