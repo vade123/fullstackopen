3bdd0ebac4ad5231eb29e7859edba2f4f868b740
Valtteri Valtanen <vade1997@gmail.com>
2020-04-09 12:35:44 +0300

    5.6 blogilistan frontend, step6

--- osa5/bloglist-frontend/src/components/Toggable.js
+++ osa5/bloglist-frontend/src/components/Toggable.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 
 const Toggable  = (props) => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const hideWhenVisible = { display: visible ? 'none' : '' };
-    const showWhenVisible = { display: visible ? '' : 'none' };
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
 
-    const toggleVisibility = () => {
-        setVisible(!visible);
-    };
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
 
-    return (
-        <div>
-            <div style={hideWhenVisible}>
-                <button onClick={toggleVisibility}>{props.buttonLabel}</button>
-            </div>
-            <div style={showWhenVisible}>
-                {props.children}
-                <button onClick={toggleVisibility}>cancel</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children} 
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  );
 };
 
 export default Toggable;
