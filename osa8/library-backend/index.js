8fd8de47191853a3d780a3ac17a1c94665a34418
Valtteri Valtanen <vade1997@gmail.com>
2021-03-19 13:33:54 +0200

    8.4: kirjailijan kirjat

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_, args) => books.filter(b => b.author === args.author),
     allAuthors: () => authors
   },
   Author: {
